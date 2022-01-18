69f804e8b473f2e31c777532af4472d42c60f898
razaizim1 <zim.sarkar5956@gmail.com>
2022-01-18 13:05:20 +0600

    String oroblem solving

---
+++ new.js
@@ -0,0 +1,9 @@
+//Find the word Sir is present or not in the string the search cannot be case sensitive.
+
+var bade = "Good morning, Sir!"
+
+
+if (bade.indexOf("Sir") !== -1) {
+    console.log("Word is present")
+} else
+    console.log("Word is not present")
